bda55ca61a453373184ac77da9f65a1890a8118e
Seyeon <ghkdtpdus95@naver.com>
2022-10-15 23:12:55 +0900

    add groundChung function

--- src/components/result/result_detail/showChung.jsx
+++ src/components/result/result_detail/showChung.jsx
@@ -51,9 +51,56 @@ const ShowChung = (props) => {
 
     }
 
+    const figureGroundChung = ()=>{
+      let groundChungArr = [];
+      let i=0;
+
+      if(ground.indexOf("인")+1==ground.indexOf("신")||ground.indexOf("인")-1==ground.indexOf("신"))
+      {
+        groundChungArr[i] = <div>인신충</div>;
+        i++;
+      }
+
+      if(ground.indexOf("사")+1==ground.indexOf("해")||ground.indexOf("사")-1==ground.indexOf("해"))
+      {
+        groundChungArr[i] = <div>사해충</div>;
+        i++;
+      }
+
+      if(ground.indexOf("자")+1==ground.indexOf("오")||ground.indexOf("자")-1==ground.indexOf("오"))
+      {
+        groundChungArr[i] = <div>자오충</div>;
+        i++;
+      }
+
+      if(ground.indexOf("묘")+1==ground.indexOf("유")||ground.indexOf("묘")-1==ground.indexOf("유"))
+      {
+        groundChungArr[i] = <div>묘유충</div>;
+        i++;
+      }
+
+      if(ground.indexOf("진")+1==ground.indexOf("술")||ground.indexOf("진")-1==ground.indexOf("술"))
+      {
+        groundChungArr[i] = <div>진술충</div>;
+        i++;
+      }
+
+      if(ground.indexOf("축")+1==ground.indexOf("미")||ground.indexOf("축")-1==ground.indexOf("미"))
+      {
+        groundChungArr[i] = <div>축미충</div>;
+        i++;
+      }
+
+      if(groundChungArr[0]==null)
+      return null;
+      return groundChungArr;
+
+    }
+
   return (
     <div>
         {figureSkyChung()}
+        {figureGroundChung()}
         </div>
   )
 }
